08c2d863f0c5c341a75ed715140c2babdf842cc3
wesandrade <wesley_reboucas@hotmail.com>
2020-06-25 14:30:05 -0300

    Implementação de View e Lista de Negociaçao

---
+++ 01_javascript_avancado/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -0,0 +1,19 @@
+class ListaNegociacoes {
+    constructor(){
+
+        this._negociacoes = [];
+
+    }
+
+    adiciona(negociacao){
+
+        this._negociacoes.push(negociacao);
+
+    }
+
+    get negociacoes(){
+
+        return [].concat(this._negociacoes);
+
+    }
+}
