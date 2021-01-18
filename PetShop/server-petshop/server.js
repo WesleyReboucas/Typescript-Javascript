69eacdd1cd2fd472ec4f7d31cb63d6a2671f235f
WesleyReboucas <wesley_reboucas@hotmail.com>
2021-01-18 16:36:05 -0300

    add petshop routes

--- PetShop/server-petshop/server.js
+++
@@ -1,9 +0,0 @@
-const customExpress = require("./config/custom-express");
-const Tabelas = require("./infraestrutura/database/tabelas");
-
-const app = customExpress();
-Tabelas.init();
-
-app.listen(4000, () => {
-  console.log("Servidor rodando na porta 4000");
-});
